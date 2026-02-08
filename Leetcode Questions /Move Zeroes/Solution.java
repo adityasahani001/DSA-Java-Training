35d2870c273f8032cb3dcc13232e85151a997498
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2026-02-08 23:57:09 +0530

    Update Solution.java

--- Leetcode Questions /Move Zeroes/Solution.java
+++ Leetcode Questions /Move Zeroes/Solution.java
@@ -37,5 +37,15 @@ class Solution {
         //         j++;
         //     }
         // }
+// for(int i=0; i<nums.length; i++){
+        //     if(nums[i] !=0){
+        //         int temp = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //         j++;
+        //     }
+        // }
+
+        // above code is also correct
 
         // above code is also correct
